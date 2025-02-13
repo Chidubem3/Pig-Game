5920f99994c2982550446357be7b2e1f1a0da253
Chidubem3 <dubemubah333@gmail.com>
2025-02-13 15:32:07 +0100

    update the winning score from 20 to 100

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-'use strict';
+"use strict";
 
 // Selecting Element
-const player0EL = document.querySelector('.player--0');
-const player1EL = document.querySelector('.player--1');
+const player0EL = document.querySelector(".player--0");
+const player1EL = document.querySelector(".player--1");
 
-const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('score--1');
-const diceEl = document.querySelector('.dice');
-const current0El = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const diceEl = document.querySelector(".dice");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
 
-const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
 // Starting conditions
 let currentScore, scores, activePlayer, playingState;
@@ -28,13 +28,13 @@ const init = function () {
   current0El.textContent = 0;
   current1El.textContent = 0;
 
-  diceEl.classList.add('hidden');
-  document.querySelector(`.player--0`).classList.remove('player--winner');
-  document.querySelector(`.player--1`).classList.remove('player--winner');
+  diceEl.classList.add("hidden");
+  document.querySelector(`.player--0`).classList.remove("player--winner");
+  document.querySelector(`.player--1`).classList.remove("player--winner");
 
   document
     .querySelector(`.player--${activePlayer}`)
-    .classList.add('player--active');
+    .classList.add("player--active");
 };
 init();
 
@@ -42,18 +42,18 @@ const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
   currentScore = 0;
-  player0EL.classList.toggle('player--active');
-  player1EL.classList.toggle('player--active');
+  player0EL.classList.toggle("player--active");
+  player1EL.classList.toggle("player--active");
 };
 
 // Rollling dice functionality
-btnRoll.addEventListener('click', function () {
+btnRoll.addEventListener("click", function () {
   if (playingState) {
     // 1. Generating a random dice roll
     const dice = Math.trunc(Math.random() * 6 + 1);
 
     // 2. Display dice
-    diceEl.classList.remove('hidden');
+    diceEl.classList.remove("hidden");
     diceEl.src = `dice-${dice}.png`;
 
     // 3. Check for rolled 1: if true
@@ -70,23 +70,23 @@ btnRoll.addEventListener('click', function () {
   }
 });
 
-btnHold.addEventListener('click', function () {
+btnHold.addEventListener("click", function () {
   if (playingState) {
     // 1. Add current score to active player's score
     scores[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // 2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // finish the game
       playingState = false;
-      diceEl.classList.add('hidden');
+      diceEl.classList.add("hidden");
       document
         .querySelector(`.player--${activePlayer}`)
-        .classList.add('player--winner');
+        .classList.add("player--winner");
       document
         .querySelector(`.player--${activePlayer}`)
-        .classList.remove('player--active');
+        .classList.remove("player--active");
     } else {
       // Else: Switch to next player
       switchPlayer();
@@ -94,4 +94,4 @@ btnHold.addEventListener('click', function () {
   }
 });
 
-btnNew.addEventListener('click', init);
+btnNew.addEventListener("click", init);
